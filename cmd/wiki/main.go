b9c6e0f382f796255cf9eff968c49d611f6e8bdc
uta8a <noumodulity@gmail.com>
2020-12-09 08:46:37 +0900

    update use postgres, add driver

--- cmd/wiki/main.go
+++ cmd/wiki/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+	"os"
 )
 
 type ResponseData struct {
@@ -12,7 +15,26 @@ type ResponseData struct {
 	Message string `json:"message"`
 }
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
+// Application State
+type State struct {
+	db *sql.DB
+}
+
+func InitEnv(k string) string {
+	// k: key v: value
+	v, ok := os.LookupEnv(k)
+	// if unset env key:value, Fatal
+	if !ok {
+		log.Fatal("Environment value is not set, key: ", k)
+	}
+	return v
+}
+func NewState(db *sql.DB) *State {
+	return &State{db: db}
+}
+func Health() {}
+
+func HealthHandler(w http.ResponseWriter, r *http.Request) {
 	// response data
 	response := ResponseData{http.StatusOK, "health check ok"}
 	res, err := json.Marshal(response)
@@ -25,13 +47,32 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(res))
 }
 
+func RegisterRoutes(db *sql.DB) {
+	_ = NewState(db)
+	http.HandleFunc("/healthcheck", HealthHandler)
+}
+
 func main() {
-	http.HandleFunc("/healthcheck", healthHandler)
+	// db init
+	dbhost := InitEnv("POSTGRES_HOST")
+	dbport := InitEnv("POSTGRES_PORT")
+	dbuser := InitEnv("POSTGRES_USER")
+	dbpass := InitEnv("POSTGRES_PASSWORD")
+	dbname := InitEnv("POSTGRES_DB")
+	dburl := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbhost, dbport, dbuser, dbpass, dbname)
+	log.Println("DB URL: ", dburl)
+	db, err := sql.Open("postgres", dburl) // return *sql.DB, error
+	if err != nil {
+		log.Fatal("DB connection failed: ", err)
+	}
+	RegisterRoutes(db)
+
+	// http.HandleFunc("/healthcheck", HealthHandler)
 	// http.HandleFunc("/privatecheck", privateHandler)
 	// http.HandleFunc("/users", userHandler)
 	// http.HandleFunc("/login", loginHandler)
 
-	err := http.ListenAndServe(":9000", nil)
+	err = http.ListenAndServe(":9000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
